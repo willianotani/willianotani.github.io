4339658f3e92f055eee32dd4a9076a0bd973868c
willianotani <148492014+willianotani@users.noreply.github.com>
2023-10-20 02:50:35 -0300

    asdad

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 const imagem_teste = document.querySelector("img");
 
+
+  
+
 imagem_teste.onclick = () =>{
     const mysrc = imagem_teste.getAttribute("src");
     if (mysrc === "images/breno.jpg"){
@@ -9,27 +12,4 @@ imagem_teste.onclick = () =>{
     }
 }
 
-let botao = document.querySelector("button");
-let mheader = document.querySelector("h1");
 
-function setUserName() {
-    const nomeguardado = prompt("DÊ-ME SEU NOME.");
-    if (!nomeguardado) {
-      setUserName();
-    } else {
-      localStorage.setItem("name", nomeguardado);
-      mheader.textContent = `ME DÁ UM PEDAÇO, ${myName}`;
-    }
-}
-  
-
-if (!localStorage.getItem("name")){
-    nomedeusuario();
-} else{
-    const nomeguardado = localStorage.getItem("name");
-    mheader.textContent = `Me dá um pedaço, ${nomeguardado}`;
-}
-
-botao.onclick = () => {
-    nomedeusuario();
-}
